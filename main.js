473ba0ac1a5af26d5523750da2331518e91edd09
Michael Nunan <michael.nunan@outlook.com>
2023-05-01 22:00:12 -0500

    added bootsrap card with button and bootstrap sorting form with button. card dissapears and form appears when card button is clicked. css included

--- main.js
+++ main.js
@@ -66,3 +66,55 @@ const accioHtml = (divId, htmlRender) => {
   const divRender = document.querySelector(divId);
   divRender.innerHTML = htmlRender;
 };
+
+// getting stuff on that dom
+
+const hatOnDom = () => {
+
+  const hatDomString =
+
+`<div class="card" id="hat-card">
+  <div class="card-header">
+  The Sorting Hat
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Oh, you may not think I'm pretty, but don't judge on what you see, I'll eat myself if you can find a smarter hat than me.</h5>
+    <p class="card-text">So put me on! Don't be afraid!
+    And don't get in a flap!
+    You're in safe hands (though I have none)
+    for I'm a thinking cap!</p>
+    <a href="#" class="btn btn-primary" id="hat-btn">Nitwit! Blubber! Oddment! Tweak!</a>
+  </div>
+</div>`
+
+accioHtml("#sorting-hat", hatDomString);
+
+}
+
+hatOnDom();
+
+
+const sortFormOnDom = () => {
+const sortFormDomString =
+`<div class="input-group mb-3" id="sort-form">
+  <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="button-addon2" id="sort-form-btn">
+  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Get Sorted</button>
+</div>`
+
+  accioHtml("#sorting-form", sortFormDomString);
+};
+
+// sortFormOnDom();
+
+const showSortingForm = document.querySelector("#button-addon2");
+
+const hideSortingForm = () =>  {
+  document.querySelector("#hat-card").style.display = "none";
+};
+
+
+
+document.querySelector("#hat-btn").addEventListener("click", () => {
+  sortFormOnDom();
+  hideSortingForm();
+});
